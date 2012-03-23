f51c1e9c30c939503370dae90a9c0c5c9a8b9ff5
Nukeador <nukeador@gmail.com>
2012-03-23 14:59:44 +0100

    Hello there

---
+++ js/functions.js
@@ -0,0 +1,8 @@
+
+          // |section| referes to this section element
+          function playVideo(section) {
+            document.querySelector("video").play();
+          }
+          function stopVideo(section) {
+            document.querySelector("video").stop();
+          }
